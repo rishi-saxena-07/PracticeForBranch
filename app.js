c41b23ba056a0b7bb876b0e16fadbd83217c27d8
rishi-saxena-07 <rishisaxenakvnkj@gmail.com>
2022-08-26 08:36:05 +0530

    Configured with MongoDB

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
+const mongoose = require("mongoose");
 const _ = require("lodash");
 
 const homeStartingContent = "Lacus vel facilisis volutpat est velit egestas dui id ornare. Semper auctor neque vitae tempus quam. Sit amet cursus sit amet dictum sit amet justo. Viverra tellus in hac habitasse. Imperdiet proin fermentum leo vel orci porta. Donec ultrices tincidunt arcu non sodales neque sodales ut. Mattis molestie a iaculis at erat pellentesque adipiscing. Magnis dis parturient montes nascetur ridiculus mus mauris vitae ultricies. Adipiscing elit ut aliquam purus sit amet luctus venenatis lectus. Ultrices vitae auctor eu augue ut lectus arcu bibendum at. Odio euismod lacinia at quis risus sed vulputate odio ut. Cursus mattis molestie a iaculis at erat pellentesque adipiscing.";
@@ -14,12 +15,23 @@ app.set('view engine', 'ejs');
 
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
-let posts = [];
+mongoose.connect("mongodb://localhost:27017/blogDB");
+
+
+const postSchema ={
+  title : String,
+  content : String
+};
+
+const Post = mongoose.model("Post", postSchema);
 
 app.get("/", function(req, res) {
-  res.render("home", {
-    homeStartingContent: homeStartingContent,
-    posts: posts
+
+  Post.find({}, function(err,posts){
+    res.render("home", {
+      homeStartingContent: homeStartingContent,
+      posts: posts
+    });
   });
 });
 
@@ -33,28 +45,34 @@ app.get("/contact", function(req, res) {
 
 app.get("/compose", function(req, res) {
   res.render("compose");
+
 });
 
 app.post("/compose", function (req, res) {
-  const post = {
-  title: req.body.composeTitle,
-  content: req.body.composePost
-  };
+  const post = new Post({
+    title : req.body.composeTitle,
+    content : req.body.composePost
+  });
 
-  posts.push(post);
-  res.redirect("/");
-});
-
-app.get("/posts/:topic", function(req, res) {
-  const requestedTitle = _.lowerCase(req.params.topic);
-
-  posts.forEach(function(post){
-    const storedTitle = _.lowerCase(post.title);
-
-    if (requestedTitle === storedTitle){
-      res.render("post" , {title : post.title, content: post.content});
+  post.save(function(err){
+    if(!err){
+      res.redirect("/");
     }
   });
+});
+
+app.get("/posts/:postId", function(req, res) {
+  const requestedPostId = req.params.postId;
+
+  Post.findOne({_id: requestedPostId}, function(err, post){
+
+   res.render("post", {
+
+     title: post.title,
+     content: post.content
+
+   });
+ });
 });
 
 
